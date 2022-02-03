c88841013bd8b018ea19df9b1c6b92adfdb8c34e
joanmgs <joan.gs@outlook.com>
2022-02-03 18:54:27 -0500

    added validations for signup, created partial for errors, added value in the hbs of signup, commented in addnotes the errors partial, added partial in the main hbs, created schema for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -14,18 +14,24 @@ router.get("/users/signup", (req, res) => {
 router.post("/users/signup", (req, res) => {
   const { name, email, password, confirm_password } = req.body;
   const errors = [];
-  if(password !== confirm_password) {
-    errors.push({ text: "Passwords doesn't match"});
+  console.log(req.body);
+  if(name.length <= 0) {
+    errors.push({ text: "Please, enter your name"});
+  };
+  if(email.length <= 0) {
+    errors.push({ text: "Please, enter your email"});
   };
   if(password.length < 4) {
     errors.push({ text: "Password should have more than 4 characters"});
   };
+  if(password !== confirm_password) {
+    errors.push({ text: "Passwords doesn't match"});
+  };
   if(errors.length > 0) {
-    res.render('/users/signup', { errors, name, email, password, confirm_password });
+    res.render('users/signup', { errors, name, email, password, confirm_password });
   } else {
     res.send("nice");
   };
-  res.send("ok");
 });
 
 export default router;
