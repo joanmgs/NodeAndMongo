6a8211652a7fe653904b2eefd13ede3c4657628a
joanmgs <joan.gs@outlook.com>
2022-02-03 18:36:55 -0500

    created signup hbs and route

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,4 +11,21 @@ router.get("/users/signup", (req, res) => {
   res.render('users/signup');
 });
 
+router.post("/users/signup", (req, res) => {
+  const { name, email, password, confirm_password } = req.body;
+  const errors = [];
+  if(password !== confirm_password) {
+    errors.push({ text: "Passwords doesn't match"});
+  };
+  if(password.length < 4) {
+    errors.push({ text: "Password should have more than 4 characters"});
+  };
+  if(errors.length > 0) {
+    res.render('/users/signup', { errors, name, email, password, confirm_password });
+  } else {
+    res.send("nice");
+  };
+  res.send("ok");
+});
+
 export default router;
